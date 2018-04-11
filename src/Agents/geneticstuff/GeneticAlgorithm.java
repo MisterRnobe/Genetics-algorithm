79abbed0283882079d0b12055a0b63f6ea908211
mcstarioni <mcstarioni@live.ru>
2018-04-11 23:01:26 +0300

    Genetic stuff

--- src/Agents/geneticstuff/GeneticAlgorithm.java
+++ src/Agents/geneticstuff/GeneticAlgorithm.java
@@ -17,18 +17,18 @@ public class GeneticAlgorithm
 
     public static void main(String[] args)
     {
-        double[] values = {12.3,34.3,546,65.0,7.45};
+        double[] values = {12.3,-34,546,65.0,7.00001};
         int nbits = 0;
         ArrayList<BitSet> bits = new ArrayList<>();
         ArrayList<String> bitsStrings = new ArrayList<>();
         for (Double i :values)
         {
-            long l = Double.doubleToLongBits(i);
+            long l = Double.doubleToRawLongBits(i);
             bitsStrings.add(Long.toBinaryString(l));
             bits.add(BitSet.valueOf(new long[]{l}));
         }
         bitsStrings.forEach(System.out::println);
-        bits.forEach(bitSet -> bitSet.toByteArray());
+        //bits.forEach(bitSet -> bitSet.toByteArray());
         //BitSet.
         //BitSet.valueOf(Arrays.stream(values).asLongStream().toArray());
         //int nbits = ;
